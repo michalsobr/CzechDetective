7ef001525d1835bb6b1913498f5fad05513b7700
Michal Sobr <michael.sobr@gmail.com>
2025-07-02 23:16:24 +0100

    Project Structure Update + small tweaks

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,19 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEditor;
+using Unity.VisualScripting;
+using System;
 
 public class MainMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public TextMeshProUGUI text;
-    public Color normalColor = Color.white;
-    public Color hoverColor = Color.yellow;
+    private Color normalColor = Color.white;
+    // Gradient hover colors
+    private Color32 topLeft = new Color32(0x00, 0x5B, 0xBF, 255);
+    private Color32 topRight = new Color32(0x00, 0x45, 0xA5, 255);
+    private Color32 bottomLeft = new Color32(0xFF, 0xFF, 0xFF, 255);
+    private Color32 bottomRight = new Color32(0xEF, 0x33, 0x40, 255);
 
     public enum ActionType { Start, Load, Exit }
     public ActionType action;
@@ -22,11 +29,13 @@ public class MainMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        text.color = hoverColor;
+        text.enableVertexGradient = true;
+        text.colorGradient = new VertexGradient(topLeft, topRight, bottomLeft, bottomRight);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        text.enableVertexGradient = false;
         text.color = normalColor;
     }
 
@@ -37,7 +46,7 @@ public class MainMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
             case ActionType.Start:
                 Debug.Log("Start Game clicked");
 
-                SceneManager.LoadScene("Pre-Arc1");
+                SceneManager.LoadScene("Base(ArcOne)");
 
                 break;
             case ActionType.Load:
