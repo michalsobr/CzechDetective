71d203ee1d1a18076a95ac35f7886e7b0bb09a47
Michal Sobr <michael.sobr@gmail.com>
2025-07-27 16:42:49 +0100

    Safety check in PopupCanvas

--- Assets/Scripts/GUI/UI/PopupCanvas.cs
+++ Assets/Scripts/GUI/UI/PopupCanvas.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,9 +7,7 @@ public abstract class PopupCanvas : MonoBehaviour
 
     protected virtual void OnEnable()
     {
-        // clear other listeners before adding the onClick listener, just in case.
-        closeButton.onClick.RemoveAllListeners();
-        closeButton.onClick.AddListener(() => Close());
+        if(closeButton) closeButton.onClick.AddListener(() => Close());
     }
 
     public virtual void Open()
